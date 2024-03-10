0a2840508015e1d5e0574cd6aa17a4ee002ad0c1
Bogdan Stolyarov <raneeeer_official@mail.ru>
2024-03-10 20:12:02 +0300

    Мы начали работу

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите размер массива:");
+        if (!int.TryParse(Console.ReadLine(), out int size) || size <= 0)
+        {
+            Console.WriteLine("Некорректный ввод. Размер массива должен быть положительным целым числом.");
+            return;
+        }
